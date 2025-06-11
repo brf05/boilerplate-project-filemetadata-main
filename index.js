4405e417a3c0f37416909c80c55875a074378414
BRIAN FABISIAK <B.FABISIAK-2022@hull.ac.uk>
2025-06-11 01:19:33 +0100

    Completed

--- index.js
+++ index.js
@@ -11,7 +11,28 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', function (req, res) {
+    const multer = require('multer');
+    const upload = multer({ dest: 'uploads/' });
 
+    upload.single('upfile')(req, res, function (err) {
+        if (err) {
+            return res.status(400).send({ error: 'File upload failed' });
+        }
+
+        if (!req.file) {
+            return res.status(400).send({ error: 'No file uploaded' });
+        }
+
+        const fileInfo = {
+            name: req.file.originalname,
+            type: req.file.mimetype,
+            size: req.file.size
+        };
+
+        res.json(fileInfo);
+    });
+});
 
 
 const port = process.env.PORT || 3000;
